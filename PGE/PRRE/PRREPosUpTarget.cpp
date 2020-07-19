b4c1d5b849ebb4f4637502f203b3af419b9e7417
proof88 <pr0o0o0o0o0o0o0o0o0o0of88@gmail.com>
2020-07-20 00:37:50 +0200

    nothing interesting

--- PGE/PRRE/PRREPosUpTarget.cpp
+++ PGE/PRRE/PRREPosUpTarget.cpp
@@ -2,7 +2,7 @@
     ###################################################################################
     PRREPosUpTarget.cpp
     This file is part of PRRE.
-    PRRE class for position, up and target vectors.
+    PRRE class for PUT (position, up and target) vectors.
     Made by PR00F88
     EMAIL : PR0o0o0o0o0o0o0o0o0o0oF88@gmail.com
     ###################################################################################
