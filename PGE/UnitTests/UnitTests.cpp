85a0c47dddffb85a1d2f5243864d16dee9da3e51
proof88 <pr0o0o0o0o0o0o0o0o0o0of88@gmail.com>
2022-09-10 17:40:12 +0200

    Update UnitTests.cpp

--- PGE/UnitTests/UnitTests.cpp
+++ PGE/UnitTests/UnitTests.cpp
@@ -138,11 +138,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     
     vector<UnitTest*> tests;
     
-    /*tests.push_back(&testPFL);
+    /**/
+    tests.push_back(&testPFL);
     tests.push_back(&testPGEcfgVariable);
     tests.push_back(&testPGEcfgFile);
     tests.push_back(&testPGESysCFG);
-    */
+    
     tests.push_back(&testWeapons);
     tests.push_back(&testPGEBullet);
     
@@ -151,7 +152,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     tests.push_back(&testSharedSettings);
     */
     
-    /*  
+    /* */ 
     tests.push_back(&testPosUpTarget);
     tests.push_back(&testPRREVector);  
     tests.push_back(&testPRREMatrix);  
@@ -189,16 +190,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     
     tests.push_back(&testPRREObject3DManager);
     tests.push_back(&testPRREObject3D);  
-    */
     
-    /*  
+    /* */ 
     tests.push_back(&testPRREWindow);
     tests.push_back(&testPRREWindow2);
-     */
+     
     
     //tests.push_back(&testPRRERendererHWfixedPipe);
     
-    /* 
+    /*
     tests.push_back(&testPRRE);
     tests.push_back(&testPRRE2);
     */
