dc51425a2ca9d1ad6d3bbc7dd967455165ba0707
proof88 <pr0o0o0o0o0o0o0o0o0o0of88@gmail.com>
2024-11-10 02:23:18 +0100

    TST: Enhance PFLFixFIFOTest

--- PGE/UnitTests/PFLFixFIFOTest.h
+++ PGE/UnitTests/PFLFixFIFOTest.h
@@ -39,6 +39,7 @@ protected:
         addSubTest("test_pop_empty", static_cast<PFNUNITSUBTEST>(&PFLFixFIFOTest::test_pop_empty));
         addSubTest("test_front_and_pop", static_cast<PFNUNITSUBTEST>(&PFLFixFIFOTest::test_front_and_pop));
         addSubTest("test_clear", static_cast<PFNUNITSUBTEST>(&PFLFixFIFOTest::test_clear));
+        addSubTest("test_begin_index_next_index", static_cast<PFNUNITSUBTEST>(&PFLFixFIFOTest::test_begin_index_next_index));
     }
 
 private:
@@ -103,7 +104,9 @@ private:
             assertFalse(fifo.full(), "full") &
             assertEquals(0u, fifo.begin_index(), "begin_index") &
             assertEquals(0u, fifo.end_index(), "end_index") &
-            assertEquals(1u, fifo.next_index(0u), "next_index")) != 0;
+            assertEquals(capacity - 1, fifo.rbegin_index(), "rbegin_index") &
+            assertEquals(1u, fifo.next_index(0u), "next_index") &
+            assertEquals(capacity - 1, fifo.prev_index(0u), "prev_index")) != 0;
     }
 
     bool test_push_back()
@@ -118,7 +121,8 @@ private:
             assertFalse(fifo.full(), "full 5") &
             assertEquals(1u, fifo.size(), "size 5") &
             assertEquals(0u, fifo.begin_index(), "begin_index 5") &
-            assertEquals(1u, fifo.end_index(), "end_index 5");
+            assertEquals(1u, fifo.end_index(), "end_index 5") &
+            assertEquals(0u, fifo.rbegin_index(), "rbegin_index 5");
 
         b &= assertTrue(fifo.push_back(3), "push 3");
         b &= assertEquals(5, fifo.front(), "front 3") &
@@ -126,7 +130,8 @@ private:
             assertFalse(fifo.full(), "full 3") &
             assertEquals(2u, fifo.size(), "size 3") &
             assertEquals(0u, fifo.begin_index(), "begin_index 3") &
-            assertEquals(2u, fifo.end_index(), "end_index 3");
+            assertEquals(2u, fifo.end_index(), "end_index 3") &
+            assertEquals(1u, fifo.rbegin_index(), "rbegin_index 3");
 
         b &= assertTrue(fifo.push_back(1), "push 1");
         b &= assertEquals(5, fifo.front(), "front 1") &
@@ -134,7 +139,8 @@ private:
             assertTrue(fifo.full(), "full 1") &
             assertEquals(3u, fifo.size(), "size 1") &
             assertEquals(0u, fifo.begin_index(), "begin_index 1") &
-            assertEquals(0u, fifo.end_index(), "end_index 1");
+            assertEquals(0u, fifo.end_index(), "end_index 1") &
+            assertEquals(2u, fifo.rbegin_index(), "rbegin_index 1");
 
         b &= assertFifoElemOrderSameAsVectorElemOrder(fifo, { 5, 3, 1 }, "check 1");
 
@@ -146,7 +152,8 @@ private:
             assertTrue(fifo.full(), "full 0") &
             assertEquals(3u, fifo.size(), "size 0") &
             assertEquals(0u, fifo.begin_index(), "begin_index 0") &
-            assertEquals(0u, fifo.end_index(), "end_index 0");
+            assertEquals(0u, fifo.end_index(), "end_index 0") &
+            assertEquals(2u, fifo.rbegin_index(), "rbegin_index 0");
         b &= assertFifoElemOrderSameAsVectorElemOrder(fifo, { 5, 3, 1 }, "check 2");
 
         // now try again with force
@@ -156,7 +163,8 @@ private:
             assertTrue(fifo.full(), "full 0 forced") &
             assertEquals(3u, fifo.size(), "size 0 forced") &
             assertEquals(1u, fifo.begin_index(), "begin_index 0 forced") &
-            assertEquals(1u, fifo.end_index(), "end_index 0 forced");
+            assertEquals(1u, fifo.end_index(), "end_index 0 forced") &
+            assertEquals(0u, fifo.rbegin_index(), "rbegin_index 0 forced");
         b &= assertFifoElemOrderSameAsVectorElemOrder(fifo, { 3, 1, 0 }, "check 3");
 
         // these are just left here to always remember
@@ -266,7 +274,12 @@ private:
         b &= assertEquals(capacity, fifo.capacity(), "cap") &
             assertEquals(0u, fifo.size(), "size") &
             assertTrue(fifo.empty(), "empty") &
-            assertFalse(fifo.full(), "full");
+            assertFalse(fifo.full(), "full") &
+            assertEquals(0u, fifo.begin_index(), "begin_index") &
+            assertEquals(0u, fifo.end_index(), "end_index") &
+            assertEquals(capacity - 1, fifo.rbegin_index(), "rbegin_index") &
+            assertEquals(1u, fifo.next_index(0u), "next_index") &
+            assertEquals(capacity - 1, fifo.prev_index(0u), "prev_index");
 
         try
         {
@@ -280,6 +293,234 @@ private:
                 std::string(e.what()),
                 "ex what");
         }
+
+        return b;
+    }
+
+    bool test_begin_index_next_index()
+    {
+        static constexpr size_t capacity = 3;
+        pfl::FixFIFO<int> fifo(capacity);
+
+        bool b = true;
+        b &= assertTrue(fifo.push_back(5), "push 5 1");
+        b &= assertTrue(fifo.push_back(3), "push 3 1");
+        b &= assertTrue(fifo.push_back(1), "push 1 1");
+
+        b &= assertEquals(0u, fifo.begin_index(), "begin_index") &
+            assertEquals(0u, fifo.end_index(), "end_index") &
+            assertEquals(capacity - 1, fifo.rbegin_index(), "rbegin_index");
+
+        if (!b)
+        {
+            return false;
+        }
+
+        // ######################################## Queue: 5 (front), 3, 1 (back)
+
+        auto iFwd = fifo.begin_index();
+        auto iRwd = fifo.rbegin_index();
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(5, fifo.underlying_array()[iFwd], "iFwd 1");
+        b &= assertEquals(1, fifo.underlying_array()[iRwd], "iRwd 1");
+
+        iFwd = fifo.next_index(iFwd);
+        iRwd = fifo.prev_index(iRwd);
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(3, fifo.underlying_array()[iFwd], "iFwd 2");
+        b &= assertEquals(3, fifo.underlying_array()[iRwd], "iRwd 2");
+
+        iFwd = fifo.next_index(iFwd);
+        iRwd = fifo.prev_index(iRwd);
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(1, fifo.underlying_array()[iFwd], "iFwd 3");
+        b &= assertEquals(5, fifo.underlying_array()[iRwd], "iRwd 3");
+        
+        fifo.pop_front();  // ######################################## Queue: 3 (front), 1 (back)
+
+        iFwd = fifo.begin_index();
+        iRwd = fifo.rbegin_index();
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(3, fifo.underlying_array()[iFwd], "iFwd 4");
+        b &= assertEquals(1, fifo.underlying_array()[iRwd], "iRwd 4");
+
+        iFwd = fifo.next_index(iFwd);
+        iRwd = fifo.prev_index(iRwd);
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(1, fifo.underlying_array()[iFwd], "iFwd 5");
+        b &= assertEquals(3, fifo.underlying_array()[iRwd], "iRwd 5");
+
+        fifo.pop_front();  // ######################################## Queue: 1
+
+        iFwd = fifo.begin_index();
+        iRwd = fifo.rbegin_index();
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(1, fifo.underlying_array()[iFwd], "iFwd 6");
+        b &= assertEquals(1, fifo.underlying_array()[iRwd], "iRwd 6");
+
+        b &= assertTrue(fifo.push_back(7), "push 7 1");
+        b &= assertTrue(fifo.push_back(9), "push 9 1");
+
+        // ######################################## Queue: 1 (front), 7, 9 (back)
+
+        iFwd = fifo.begin_index();
+        iRwd = fifo.rbegin_index();
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(1, fifo.underlying_array()[iFwd], "iFwd 7");
+        b &= assertEquals(9, fifo.underlying_array()[iRwd], "iRwd 7");
+
+        iFwd = fifo.next_index(iFwd);
+        iRwd = fifo.prev_index(iRwd);
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(7, fifo.underlying_array()[iFwd], "iFwd 8");
+        b &= assertEquals(7, fifo.underlying_array()[iRwd], "iRwd 8");
+
+        iFwd = fifo.next_index(iFwd);
+        iRwd = fifo.prev_index(iRwd);
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(9, fifo.underlying_array()[iFwd], "iFwd 9");
+        b &= assertEquals(1, fifo.underlying_array()[iRwd], "iRwd 9");
+
+        fifo.pop_front();  // ######################################## Queue: 7 (front), 9 (back)
+
+        iFwd = fifo.begin_index();
+        iRwd = fifo.rbegin_index();
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(7, fifo.underlying_array()[iFwd], "iFwd 10");
+        b &= assertEquals(9, fifo.underlying_array()[iRwd], "iRwd 10");
+
+        iFwd = fifo.next_index(iFwd);
+        iRwd = fifo.prev_index(iRwd);
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(9, fifo.underlying_array()[iFwd], "iFwd 11");
+        b &= assertEquals(7, fifo.underlying_array()[iRwd], "iRwd 11");
+
+        fifo.pop_front();  // ######################################## Queue: 9
+
+        iFwd = fifo.begin_index();
+        iRwd = fifo.rbegin_index();
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(9, fifo.underlying_array()[iFwd], "iFwd 12");
+        b &= assertEquals(9, fifo.underlying_array()[iRwd], "iRwd 12");
+
+        fifo.pop_front();  // ######################################## Queue: empty
+
+        b &= assertTrue(fifo.push_back(5), "push 5 2");
+        b &= assertTrue(fifo.push_back(3), "push 3 2");
+        b &= assertTrue(fifo.push_back(1), "push 1 2");
+
+        // ######################################## Queue: 5 (front), 3, 1 (back)
+
+        iFwd = fifo.begin_index();
+        iRwd = fifo.rbegin_index();
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(5, fifo.underlying_array()[iFwd], "iFwd 13");
+        b &= assertEquals(1, fifo.underlying_array()[iRwd], "iRwd 13");
+
+        iFwd = fifo.next_index(iFwd);
+        iRwd = fifo.prev_index(iRwd);
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(3, fifo.underlying_array()[iFwd], "iFwd 14");
+        b &= assertEquals(3, fifo.underlying_array()[iRwd], "iRwd 14");
+
+        iFwd = fifo.next_index(iFwd);
+        iRwd = fifo.prev_index(iRwd);
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(1, fifo.underlying_array()[iFwd], "iFwd 15");
+        b &= assertEquals(5, fifo.underlying_array()[iRwd], "iRwd 15");
+
+        fifo.pop_front();  // ######################################## Queue: 3 (front), 1 (back)
+
+        iFwd = fifo.begin_index();
+        iRwd = fifo.rbegin_index();
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(3, fifo.underlying_array()[iFwd], "iFwd 16");
+        b &= assertEquals(1, fifo.underlying_array()[iRwd], "iRwd 16");
+
+        iFwd = fifo.next_index(iFwd);
+        iRwd = fifo.prev_index(iRwd);
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(1, fifo.underlying_array()[iFwd], "iFwd 17");
+        b &= assertEquals(3, fifo.underlying_array()[iRwd], "iRwd 17");
+
+        b &= assertTrue(fifo.push_back(7), "push 7 2");  // ######################################## Queue: 3 (front), 1, 7 (back)
+
+        iFwd = fifo.begin_index();
+        iRwd = fifo.rbegin_index();
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(3, fifo.underlying_array()[iFwd], "iFwd 18");
+        b &= assertEquals(7, fifo.underlying_array()[iRwd], "iRwd 18");
+
+        iFwd = fifo.next_index(iFwd);
+        iRwd = fifo.prev_index(iRwd);
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(1, fifo.underlying_array()[iFwd], "iFwd 19");
+        b &= assertEquals(1, fifo.underlying_array()[iRwd], "iRwd 19");
+
+        iFwd = fifo.next_index(iFwd);
+        iRwd = fifo.prev_index(iRwd);
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(7, fifo.underlying_array()[iFwd], "iFwd 20");
+        b &= assertEquals(3, fifo.underlying_array()[iRwd], "iRwd 20");
+
+        fifo.pop_front();  // ######################################## Queue: 1 (front), 7 (back)
+
+        iFwd = fifo.begin_index();
+        iRwd = fifo.rbegin_index();
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(1, fifo.underlying_array()[iFwd], "iFwd 21");
+        b &= assertEquals(7, fifo.underlying_array()[iRwd], "iRwd 21");
+
+        iFwd = fifo.next_index(iFwd);
+        iRwd = fifo.prev_index(iRwd);
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(7, fifo.underlying_array()[iFwd], "iFwd 22");
+        b &= assertEquals(1, fifo.underlying_array()[iRwd], "iRwd 22");
+
+        b &= assertTrue(fifo.push_back(9), "push 9 2");  // ######################################## Queue: 1 (front), 7, 9 (back)
+
+        iFwd = fifo.begin_index();
+        iRwd = fifo.rbegin_index();
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(1, fifo.underlying_array()[iFwd], "iFwd 23");
+        b &= assertEquals(9, fifo.underlying_array()[iRwd], "iRwd 23");
+
+        iFwd = fifo.next_index(iFwd);
+        iRwd = fifo.prev_index(iRwd);
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(7, fifo.underlying_array()[iFwd], "iFwd 24");
+        b &= assertEquals(7, fifo.underlying_array()[iRwd], "iRwd 24");
+
+        iFwd = fifo.next_index(iFwd);
+        iRwd = fifo.prev_index(iRwd);
+        assert(iFwd < capacity);
+        assert(iRwd < capacity);
+        b &= assertEquals(9, fifo.underlying_array()[iFwd], "iFwd 25");
+        b &= assertEquals(1, fifo.underlying_array()[iRwd], "iRwd 25");
 
         return b;
     }
